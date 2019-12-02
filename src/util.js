6d2c736a872edefaa57156eea35daa1837ff8f57
polinayermolenko <daniel24111993@mail.ru>
2019-12-02 22:55:55 +0300

    Создала моки и заменила рыбные данные на настоящие

---
+++ src/util.js
@@ -0,0 +1,11 @@
+const custTimeFormat = (value) => {
+  return value < 10 ? `0${value}` : String(value);
+};
+
+export const formatTime = (date) => {
+  const hours = custTimeFormat(date.getHours() % 12);
+  const minutes = custTimeFormat(date.getMinutes());
+  const interval = date.getHours() < 11 ? `AM` : `PM`;
+
+  return `${hours}: ${minutes} ${interval}`;
+};
