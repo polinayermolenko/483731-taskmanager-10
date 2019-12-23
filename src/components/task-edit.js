7dc3cfd8787a647a848c80be52605de9adeda0f5
polinayermolenko <daniel24111993@mail.ru>
2019-12-24 00:13:59 +0300

    Реализовала наследование компонента формы от абстрактного класса

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -1,5 +1,6 @@
+import AbstractComponent from './abstract-component.js';
 import {Colors, Days, MonthNames} from '../const.js';
-import {formatTime, createElement} from '../util.js';
+import {formatTime} from '../util.js';
 
 const createRepeatingDaysMarkup = (days, repeatingDays) => {
   return days
@@ -169,25 +170,14 @@ const createTaskEditTemplate = (task) => {
 };
 
 
-export default class TaskEdit {
+export default class TaskEdit extends AbstractComponent {
   constructor(task) {
+    super();
+
     this._task = task;
-    this._element = null;
   }
 
   getTemplate() {
     return createTaskEditTemplate(this._task);
-  }
-
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
   }
 }
