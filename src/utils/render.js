f0a3dbd794ce70332c976e44e68d574d959b4b7a
polinayermolenko <daniel24111993@mail.ru>
2019-12-25 15:57:35 +0300

    Отрефакторила функцию render

--- src/utils/render.js
+++ src/utils/render.js
@@ -10,14 +10,14 @@ export const createElement = (template) => {
   return parent.firstChild;
 };
 
-export const render = (container, element, place) => {
+export const render = (container, component, place) => {
   switch (place) {
     case RenderPosition.AFTERBEGIN:
-      container.prepend(element);
+      container.prepend(component.getElement());
       break;
 
     case RenderPosition.BEFOREEND:
-      container.append(element);
+      container.append(component.getElement());
       break;
   }
 };
